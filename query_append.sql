da945c9062801f8b4059d365d537b291dc6f7cbb
xtern0o <dos30028@gmail.com>
2025-04-10 09:56:07 +0300

    [fix]

--- query_append.sql
+++ query_append.sql
@@ -33,14 +33,6 @@ INSERT INTO location (name, parent_location_id, biom_id) VALUES
     ('крутой пляж', 1, 5), 
     ('лагуна с утнконосами', 2, 2);
 
-INSERT INTO dinosaur (name, gender, age, dino_type_id, current_swarm_id, parent_dino_id, current_location_id) VALUES 
-    (DEFAULT, 'f', 30, 2, 1, NULL, 1),
-    ('максим', 'm', 19, 1, 1, 1, 3),
-    ('еще утконос', 'm', 4430, 2, 1, 2, 2),
-    ('Грант', 'm', 20, 3, 2, 3, 2),
-    ('Грант2', 'm', 20, 3, 1, 3, 2),
-    ('Гран4т', 'm', 20, 3, 1, 3, 2);
-
 INSERT INTO swarm (name, location_id) VALUES 
     ('крутая стая', 1), 
     ('пустая стая', 1),
@@ -62,3 +54,11 @@ INSERT INTO dinotype_to_diet (dinotype_id, diet_id) VALUES
     (1, 4),
     (2, 2),
     (2, 3);
+
+INSERT INTO dinosaur (name, gender, age, dino_type_id, current_swarm_id, parent_dino_id, current_location_id) VALUES 
+    (DEFAULT, 'f', 30, 2, 1, NULL, 1),
+    ('максим', 'm', 19, 1, 1, 1, 3),
+    ('еще утконос', 'm', 4430, 2, 1, 2, 2),
+    ('Грант', 'm', 20, 3, 2, 3, 2),
+    ('Грант2', 'm', 20, 3, 1, 3, 2),
+    ('Гран4т', 'm', 20, 3, 1, 3, 2);
