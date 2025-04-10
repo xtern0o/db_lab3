dd20a701a3b63624a3c8e23a55ba549237edd3b3
xtern0o <dos30028@gmail.com>
2025-04-10 09:57:08 +0300

    fiix

--- query_append.sql
+++ query_append.sql
@@ -38,6 +38,14 @@ INSERT INTO swarm (name, location_id) VALUES
     ('пустая стая', 1),
     ('Grant party', 2);
 
+INSERT INTO dinosaur (name, gender, age, dino_type_id, current_swarm_id, parent_dino_id, current_location_id) VALUES 
+    (DEFAULT, 'f', 30, 2, 1, NULL, 1),
+    ('максим', 'm', 19, 1, 1, 1, 3),
+    ('еще утконос', 'm', 4430, 2, 1, 2, 2),
+    ('Грант', 'm', 20, 3, 2, 3, 2),
+    ('Грант2', 'm', 20, 3, 1, 3, 2),
+    ('Гран4т', 'm', 20, 3, 1, 3, 2);
+
 INSERT INTO dino_to_action (dino_id, action_id) VALUES
     (1, 1),
     (1, 3),
@@ -54,11 +62,3 @@ INSERT INTO dinotype_to_diet (dinotype_id, diet_id) VALUES
     (1, 4),
     (2, 2),
     (2, 3);
-
-INSERT INTO dinosaur (name, gender, age, dino_type_id, current_swarm_id, parent_dino_id, current_location_id) VALUES 
-    (DEFAULT, 'f', 30, 2, 1, NULL, 1),
-    ('максим', 'm', 19, 1, 1, 1, 3),
-    ('еще утконос', 'm', 4430, 2, 1, 2, 2),
-    ('Грант', 'm', 20, 3, 2, 3, 2),
-    ('Грант2', 'm', 20, 3, 1, 3, 2),
-    ('Гран4т', 'm', 20, 3, 1, 3, 2);
